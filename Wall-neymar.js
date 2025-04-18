80310e90d77c1163ab64aae5afcc824992a499a4
kerimli911 <96395537+kerimli911@users.noreply.github.com>
2025-04-18 11:22:39 +0400

    Wall-neymar.js

--- Wall-neymar.js
+++ Wall-neymar.js
@@ -4,10 +4,10 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     await axios.get(`https://raw.githubusercontent.com/Guru322/api/Guru/BOT-JSON/Messi.json`)
   ).data
   let url = await res[Math.floor(res.length * Math.random())]
-  conn.sendFile(m.chat, url, 'error.jpg', `*Messi*`, m)
+  conn.sendFile(m.chat, url, 'error.jpg', `*Neymarrr*`, m)
 }
-//conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
-handler.help = ['messi']
+//conn.sendButton(m.chat, "*Neymarrr*", author, url, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
+handler.help = ['neymar']
 handler.tags = ['img']
-handler.command = /^(messi)$/i
+handler.command = /^(neymar)$/i
 export default handler
